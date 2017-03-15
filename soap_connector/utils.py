63c3f83525fd97ea95e6aae5b082d89dc046ad51
gmork2 <gmork.02@gmail.com>
2017-03-15 22:59:31 +0100

    New NoDupOrderedDict class that rejects duplicate definitions

--- soap_connector/utils.py
+++ soap_connector/utils.py
@@ -1,2 +1,15 @@
+from collections import OrderedDict
 
+
+class NoDupOrderedDict(OrderedDict):
+    """ Rejects duplicate definitions """
+    
+    def __init__(self, clsname):
+        self.clsname = clsname
+        super().__init__()
+        
+    def __setitem__(self, name, value):
+        if name in self:
+            raise TypeError('{} already defined in {}'.format(name, self.clsname))
+        super().__setitem__(name, value)
 
