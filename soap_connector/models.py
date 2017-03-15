aec60db06525fe33e7e35eeb2ea4532863b40f1a
gmork2 <gmork.02@gmail.com>
2017-03-15 23:07:45 +0100

    New ModelFactory class in models

--- soap_connector/models.py
+++ soap_connector/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+import keyword
+import re
 
-# Create your models here.
+from django.db import models
+from django.db.models.base import ModelBase
+
+
+class ModelFactory(ModelBase):
+    @classmethod
+    def __prepare__(cls, name, bases, *, debug=False, synchronize=False):
+        # Convert to valid class name
+        to_var = lambda v: re.sub('\W|^(?=\d)','_', v)
+        if name.isidentifier():
+            name = to_var(name)
+        name += ('', '_')[keyword.iskeyword(name)]
+
