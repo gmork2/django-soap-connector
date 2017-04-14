756746acb71c696a4d1c2a4ccae2d0f080a45b6f
gmork2 <gmork.02@gmail.com>
2017-04-14 23:27:47 +0200

    Defined a init/call method for ModelFactory

--- soap_connector/models.py
+++ soap_connector/models.py
@@ -14,3 +14,18 @@ class ModelFactory(ModelBase):
             name = to_var(name)
         name += ('', '_')[keyword.iskeyword(name)]
 
+    def __init__(self, name, bases, ns, *, debug=False, synchronize=False):
+        # Create cache for class instances
+        super().__init__(name, bases, ns)
+        self.__cache = weakref.WeakValueDictionary()
+
+    def __call__(self, *args):
+        if args in self.__cache:
+            logger.info(self.__cache[args])
+            return self.__cache[args]
+        else:
+            obj = super().__call__(*args)
+            self.__cache[args] = obj
+        return obj
+
+
