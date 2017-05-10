47fa8b48facd20ff987a206cb715781b7921fce3
gmork2 <gmork.02@gmail.com>
2017-05-10 23:52:07 +0200

    Implementing dump_wsdl command for print a wsdl document

--- soap_connector/management/commands/wsdl.py
+++ soap_connector/management/commands/wsdl.py
@@ -1,4 +1,7 @@
+import operator
+
 from django.core.management.base import BaseCommand, CommandError
+from zeep.client import Client
 
 
 class Command(BaseCommand):
@@ -8,4 +11,42 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        pass
+        wsdl = options['wsdl']
+        if wsdl:
+            client = Client(wsdl)
+            self.stdout.write('')
+            self.stdout.write("Prefixes:")
+            for prefix, namespace in self.types.prefix_map.items():
+                self.stdout.write(' ' * 4, '%s: %s' % (prefix, namespace))
+    
+            self.stdout.write('')
+            self.stdout.write("Global elements:")
+            for elm_obj in sorted(client.wsdl.types.elements, key=lambda k: k.qname):
+                value = elm_obj.signature(schema=client.wsdl.types)
+                self.stdout.write(' ' * 4, value)
+    
+            self.stdout.write('')
+            self.stdout.write("Global types:")
+            for type_obj in sorted(client.wsdl.types.types, key=lambda k: k.qname or ''):
+                value = type_obj.signature(schema=client.wsdl.types)
+                self.stdout.write(' ' * 4, value)
+    
+            self.stdout.write('')
+            self.stdout.write("Bindings:")
+            for binding_obj in sorted(client.wsdl.bindings.values(), key=lambda k: str(k)):
+                self.stdout.write(' ' * 4, str(binding_obj))
+    
+            print('')
+            for service in client.wsdl.services.values():
+                self.stdout.write(str(service))
+                for port in service.ports.values():
+                    self.stdout.write(' ' * 4, str(port))
+                    self.stdout.write(' ' * 8, 'Operations:')
+    
+                    operations = sorted(
+                        port.binding._operations.values(),
+                        key=operator.attrgetter('name'))
+    
+                    for operation in operations:
+                        self.stdout.write('%s%s' % (' ' * 12, str(operation)))
+                    self.stdout.write('')
