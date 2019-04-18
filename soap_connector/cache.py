b78dc59b853b29bc6916d61dc451bcd6d7b68c71
gmork2 <gmork.02@gmail.com>
2019-04-18 20:29:53 +0200

    Added cache.Cache class wrapping django's cache

--- soap_connector/cache.py
+++ soap_connector/cache.py
@@ -1,10 +1,11 @@
 import logging
-from typing import Dict, Union
+from typing import Dict, List, Any, Union, Optional
 
 from django.core.cache import cache
 
 logger = logging.getLogger(__name__)
 
+ObjectList = List[Optional[dict]]
 Context = Dict[
     str,
     Union[
@@ -24,3 +25,40 @@ def make_key(context, sufix: str) -> str:
         sufix
     ])
 
+
+class Cache(object):
+    """
+    Cache protocol wrapping django's cache.
+    """
+    def __init__(self, context: Context):
+        """
+
+        :param context:
+        """
+        self.key: Optional[str] = None
+        self.context = context
+
+    def __getitem__(self, item):
+        """
+
+        :param item:
+        :return:
+        """
+        return cache[item]
+
+    def __setitem__(self, key, value):
+        """
+
+        :param key:
+        :param value:
+        :return:
+        """
+        cache[key] = value
+
+    def __delitem__(self, key):
+        """
+
+        :param key:
+        :return:
+        """
+        del cache[key]
