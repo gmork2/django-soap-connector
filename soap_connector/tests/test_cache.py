2268a10153ac9b4bcea7fb4acdd7989f062e7be1
gmork2 <gmork.02@gmail.com>
2019-04-18 20:11:51 +0200

    Added CacheTestCase prototype to tests.test_cache

---
+++ soap_connector/tests/test_cache.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+
+class CacheTestCase(TestCase):
+    """
+
+    """
+    def setUp(self):
+        """
+
+        :return:
+        """
+        self.context = {}
+
+    def test_get_item_from_empty_cache(self):
+        """
+
+        :return:
+        """
+        pass
+
+    def tearDown(self):
+        """
+
+        :return:
+        """
+        pass
