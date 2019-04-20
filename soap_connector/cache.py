05c6781e3a25f212d62afb5016df04ad6714e404
gmork2 <gmork.02@gmail.com>
2019-04-20 21:32:48 +0200

    Added cache.Registry class to reference the current cache entries

--- soap_connector/cache.py
+++ soap_connector/cache.py
@@ -26,6 +26,72 @@ def make_key(context, sufix: str) -> str:
     ])
 
 
+class Registry(object):
+    """
+
+    """
+    def __init__(self, context):
+        """
+
+        :return:
+        """
+        self.key: str = make_key(context, 'versions')
+        self.cls: type = context['view'].object_class
+
+        data = self.retrieve()
+
+        if isinstance(data, dict):
+            cache.set(self.key, data)
+
+    def retrieve(self) -> Union[Dict[str, list], List[int]]:
+        """
+
+        :return:
+        """
+
+        registry: dict = cache.get(self.key)
+
+        if registry:
+            return registry.get(self.cls.__name__, [])
+        return {self.cls.__name__: []}
+
+    def insert(self, version: int) -> None:
+        """
+
+        :param version:
+        :return:
+        """
+        versions: List[int] = self.retrieve()
+
+        if version not in versions:
+            versions.append(version)
+            self.update(versions)
+
+    def remove(self, version: int) -> None:
+        """
+
+        :param version:
+        :return:
+        """
+        versions: List[int] = self.retrieve()
+
+        if version in versions:
+            versions.remove(version)
+            self.update(versions)
+
+    def update(self, versions: List[int]) -> None:
+        """
+
+        :param versions:
+        :return:
+        """
+        data = {
+            **cache.get(self.key),
+            **{self.cls.__name__: versions}
+        }
+        cache.set(self.key, data)
+
+
 class Cache(object):
     """
     Cache protocol wrapping django's cache.
