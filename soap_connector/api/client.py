e1ed386cde32fedf2ddd22d2c35afc3971e3035f
gmork2 <gmork.02@gmail.com>
2019-04-26 17:16:35 +0200

    New api.client.ConnectorView parent class for views that use connector

--- soap_connector/api/client.py
+++ soap_connector/api/client.py
@@ -1,9 +1,15 @@
 import logging
+from typing import ClassVar
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.request import Request
 
 from zeep.client import Client
 
 from soap_connector.serializers import ClientSerializer
 from soap_connector.api.base import BaseAPIView
+from soap_connector.connector import Connector
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +20,48 @@ class ClientView(BaseAPIView):
     """
     serializer_class = ClientSerializer
     object_class = Client
+
+
+class ConnectorView(ClientView):
+    """
+
+    """
+    source_name: ClassVar[str] = ''
+    object_pk_name: ClassVar[str] = ''
+
+    @property
+    def allowed_methods(self):
+        """
+
+        :return:
+        """
+        return ['GET']
+
+    def list(self, request: Request, **kwargs) -> Response:
+        """
+
+        :param request:
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def get(self, request: Request, **kwargs) -> Response:
+        """
+
+        :param request:
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def save(self, object_list):
+        """
+
+        :param object_list:
+        :return:
+        """
+        pass
 
 
 client = ClientView.as_view()
