3f49470c31ea75bb17787460d2aec0b5d620835f
gmork2 <gmork.02@gmail.com>
2019-04-26 17:59:09 +0200

    Added global_type, global_element, prefix and binding endpoints to urls

--- soap_connector/api/__init__.py
+++ soap_connector/api/__init__.py
@@ -1,7 +1,10 @@
 from .base import root
 from .settings import settings
-from .client import client
+from .client import client, global_type, global_element, prefix, binding
 from .wsse import signature, username_token
 
 
-__all__ = ['root', 'settings', 'client', 'signature', 'username_token']
+__all__ = [
+    'root', 'settings', 'client', 'global_type', 'global_element', 'prefix',
+    'binding', 'signature', 'username_token',
+]
