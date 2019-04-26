0c1f3a9535051d75591e118521e736ddb41ee834
gmork2 <gmork.02@gmail.com>
2019-04-26 17:00:39 +0200

    New serializers.client.ClientSerializer class

---
+++ soap_connector/serializers/client.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .base import BaseSerializer
+from soap_connector.fields import HyperlinkedField
+
+
+class ClientSerializer(BaseSerializer):
+    """
+
+    """
+    wsdl = serializers.URLField()
+    # wsse = serializers.ForeignKey()
+    transport = None
+    service_name = serializers.CharField(required=False)
+    port_name = serializers.CharField(required=False)
+    # settings = SettingsSerializer(required=False)
+    settings = serializers.IntegerField(min_value=1, required=False)
+    url = HyperlinkedField(view_name='soap_connector:client_detail')
+
+    class Meta:
+        fields = ['wsdl', 'service_name', 'port_name', 'settings']
