011bd66bc41954560726204687a4ac3b3f975dfc
gmork2 <gmork.02@gmail.com>
2019-04-30 17:29:37 +0200

    Implemented api.service.ServiceView.save method

--- soap_connector/api/service.py
+++ soap_connector/api/service.py
@@ -19,7 +19,18 @@ class ServiceView(ConnectorView):
         :param object_list:
         :return:
         """
-        pass
+        for _service in object_list:
+            self.set_context(Service)
+            self.cache[_service['pk']] = _service
+
+            for _port in _service['ports']:
+                self.set_context(Port)
+                self.cache[_port['pk']] = _port
+
+                for _operation in _port['operations']:
+                    self.set_context(Operation)
+                    self.cache[_operation['pk']] = _operation
+        self.set_context(Service)
 
 
 service = ServiceView.as_view()
