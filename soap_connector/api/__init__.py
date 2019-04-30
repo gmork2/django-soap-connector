df5c8fd7b122da441fa146b01b839f572847c6ee
gmork2 <gmork.02@gmail.com>
2019-04-30 18:12:56 +0200

    Fixed port and operation import error

--- soap_connector/api/__init__.py
+++ soap_connector/api/__init__.py
@@ -2,6 +2,8 @@ from .base import root
 from .settings import settings
 from .client import client, global_type, global_element, prefix, binding
 from .service import service
+from .port import port
+from .operation import operation
 from .wsse import signature, username_token
 
 
