cdc672814184dcadcac26996ab096d3d642e133c
gmork2 <gmork.02@gmail.com>
2019-05-08 17:06:40 +0200

    New serializers.operation.OperationSerializer class

--- soap_connector/serializers/__init__.py
+++ soap_connector/serializers/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseSerializer
 from .settings import SettingsSerializer
 from .client import ClientSerializer
+from .operation import OperationSerializer
 from .wsse import SignatureSerializer, UsernameTokenSerializer
---
+++ soap_connector/serializers/operation.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class OperationSerializer(serializers.Serializer):
+    """
+
+    """
+    pass
