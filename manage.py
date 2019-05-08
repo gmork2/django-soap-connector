8eecedda7dea1e9dfa4ff4cffbdd9834cfeb6905
gmork2 <gmork.02@gmail.com>
2019-05-08 17:05:32 +0200

    Updated manage.py

--- manage.py
+++ manage.py
@@ -3,15 +3,28 @@ import sys
 from django.conf import settings
 from django.core.management import execute_from_command_line
 
+from django.conf.urls import url, include
+
+
 settings.configure(
     DEBUG=True,
     SECRET_KEY='A-random-secret-key!',
     ROOT_URLCONF=sys.modules[__name__],
-    INSTALLED_APPS = (
-        'django.contrib.admin',
+    INSTALLED_APPS=(
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'rest_framework',
         'soap_connector'
     ),
-)
+    REST_FRAMEWORK={
+        'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.AllowAny',
+        )
+    })
+
+urlpatterns = [
+    url(r'^api/', include(('soap_connector.urls', 'soap_connector'), namespace='soap_connector')),
+]
 
 if __name__ == '__main__':
     execute_from_command_line(sys.argv)
