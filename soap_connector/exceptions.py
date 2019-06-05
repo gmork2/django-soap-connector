b98ebcbedf6788b80f64743d4f7160e67cf1d7cc
gmork2 <gmork.02@gmail.com>
2019-06-05 17:03:27 +0200

    New exceptions.CacheError exception

--- soap_connector/exceptions.py
+++ soap_connector/exceptions.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Type, Any
 
 
 class ConnectorError(Exception):
@@ -10,3 +10,16 @@ class ConnectorError(Exception):
             msg = f"An error occurred with connector {instance}"
         super().__init__(msg)
         self.instance = instance
+
+
+class CacheError(Exception):
+    """
+    Exception for cache access error.
+    """
+    def __init__(self, instance: Type, cache: "Cache", pk: int):
+        super().__init__(
+            instance,
+            f"{instance}: I can't retrieve item for {cache.registry.cls} with pk={pk}"
+        )
+        self.cache = cache
+        self.pk = pk
