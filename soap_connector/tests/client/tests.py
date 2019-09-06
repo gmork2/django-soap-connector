98fbefe1b0dd387d837afe084853edcb3252ecf8
gmork2 <gmork.02@gmail.com>
2019-09-06 19:35:58 +0200

    Added a new client package tests

---
+++ soap_connector/tests/client/tests.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AnonymousUser
+from rest_framework.reverse import reverse
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+
+
+class ClientViewTestCase(APITestCase):
+    """
+
+    """
+    def setUp(self):
+        """
+
+        :return:
+        """
+        self.url = reverse("soap_connector:client_list")
+
+        self.request = APIRequestFactory().get(self.url)
+        self.request.user = AnonymousUser()
+
+    def test_simple(self):
+        """
+
+        :return:
+        """
+        url = reverse("soap_connector:root")
+        response = self.client.get(url)
+
+        self.assertIn(
+            self.request.build_absolute_uri(),
+            response.data.values()
+        )
+
+    def test_get_empty_client_list(self):
+        """
+
+        :return:
+        """
+        response = self.client.get(self.url)
+        self.assertEqual(404, response.status_code)
+
+    def tearDown(self):
+        """
+
+        :return:
+        """
+        pass
