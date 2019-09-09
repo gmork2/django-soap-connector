96341c9e3a3280bfd18681be376b4aa4f2181e07
gmork2 <gmork.02@gmail.com>
2019-09-09 20:50:08 +0200

    Removed root code

--- soap_connector/tests/api/tests.py
+++ soap_connector/tests/api/tests.py
@@ -1,57 +1,13 @@
-from urllib.parse import urlparse, ParseResult
 from unittest import skip
 from copy import copy
 
 from django.test import TestCase
-from django.urls import resolve, ResolverMatch
 from django.contrib.auth.models import AnonymousUser
 from rest_framework.reverse import reverse
 from rest_framework import serializers
 from rest_framework.test import APIRequestFactory
 
-from soap_connector.api.base import URL_NAMES
 from soap_connector.tests.api.utils import DummyView, BaseSerializer, set_name
-
-
-class RootTestCase(TestCase):
-    """
-
-    """
-    def setUp(self):
-        """
-
-        :return:
-        """
-        self.url = reverse("soap_connector:root")
-        self.response = self.client.get(self.url)
-
-    def test_simple(self):
-        """
-
-        :return:
-        """
-        self.assertEqual(200, self.response.status_code)
-
-    def test_url_names(self):
-        """
-
-        :return:
-        """
-        self.assertSetEqual(
-            set(self.response.data.keys()),
-            set(URL_NAMES)
-        )
-
-    def test_endpoints(self):
-        """
-        Endpoint url path must correspond to a view
-        function .
-
-        :return:
-        """
-        for url in self.response.data.values():
-            link = ParseResult('', '', *urlparse(url)[2:]).geturl()
-            self.assertIsInstance(resolve(link), ResolverMatch)
 
 
 class BaseTestCase(TestCase):
